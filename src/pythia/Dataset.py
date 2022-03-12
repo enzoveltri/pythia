0793640010208e7335c06d02db875de9064aa6b4
antoniovizzuso <46349767+antoniovizzuso@users.noreply.github.com>
2022-03-12 17:52:08 +0100

    fix previous commit

---
+++ src/pythia/Dataset.py
@@ -0,0 +1,220 @@
+import json
+
+import pandas as pd
+from pandas.core.dtypes.common import is_numeric_dtype
+
+from src.pythia.Attribute import Attribute
+from src.pythia.Constants import STRATEGY_SCHEMA, STRATEGY_SCHEMA_WITH_DATA_SAMPLE, STRATEGY_PAIRWISE_COMBINATION, \
+    STRATEGY_PAIRWISE_PERMUTATION, CATEGORICAL, NUMERICAL, INDEX
+from src.pythia.Profiling import getCompositeKeys, getFDs, getAmbiguousAttribute
+from src.pythia.StringUtils import normalizeString
+from itertools import combinations, permutations
+
+class Dataset:
+
+    def __init__(self, datasetName):
+        self.datasetName = datasetName
+        self.pk = None
+        self.fds = []
+        self.compositeKeys = []
+        self.ambiguousAttribute = []
+
+    def initDataframe(self, file):
+        self.dataframe = self._loadDataFrame(file)
+        self.attributes = self._getAttributesFromDF(self.dataframe)
+        self.nameToAttribute = self._dictNameToAttribute()
+
+
+    def findCompositeKeys(self, maxSizeKeys, ckMinimal):
+        return self.findCompositeKeys(self, self.dataframe, maxSizeKeys, ckMinimal)
+
+    def findCompositeKeys(self, dataframe, maxSizeKeys, ckMinimal):
+        compositeKeysName = getCompositeKeys(dataframe, maxSizeKeys)
+        self.compositeKeys = []
+        for ck in compositeKeysName:
+            ckAttr = []
+            for attrName in ck:
+                if attrName == self.pk.name:
+                    break
+                attr = self.nameToAttribute[attrName]
+                ckAttr.append(attr)
+            self.compositeKeys.append(ckAttr)
+        if ckMinimal and len(self.compositeKeys) > 0:
+            self.compositeKeys = [self.compositeKeys[0]]
+
+    def findPK(self):
+        rows = self.dataframe.shape[0]
+        for column in self.dataframe.columns:
+            uniqueValuesInColumn = len(self.dataframe[column].unique())
+            if uniqueValuesInColumn == rows:
+                self.pk = self.nameToAttribute[column]
+                break
+        if self.pk is None:
+            self.pk = INDEX
+        ## update also the dataframe
+        self.dataframe.set_index(self.pk.name, inplace=True)
+
+    def allCategorical(self, attributes):
+        for attr in attributes:
+            if attr.type != CATEGORICAL:
+                return False
+        return True
+
+    def pruneCKsByType(self, useNumerical=False):
+        if useNumerical == True:
+            return
+        compositeKeysCategorical = []
+        for ck in self.compositeKeys:
+            if self.allCategorical(ck):
+                compositeKeysCategorical.append(ck)
+        self.compositeKeys = compositeKeysCategorical
+
+    def findFDs(self, df, dropFiles, tempFolder = "./taneFolder/"):
+        fdsName = getFDs(df, self.attributes, dropFiles, tempFolder)
+        self.fds = []
+        for fd in fdsName:
+            fdAttr = []
+            for attrName in fd:
+                attr = self.nameToAttribute[attrName]
+                fdAttr.append(attr)
+            self.fds.append(fdAttr)
+
+    def extendFDs(self, rowMeaning):
+        extendedFDs = []
+        for fd in self.fds:
+            efd = (fd, rowMeaning)
+            extendedFDs.append(efd)
+        self.fds = extendedFDs
+
+    def getCompositeKeys(self):
+        return self.compositeKeys
+
+    def getPk(self):
+        return self.pk
+
+    def getDatasetName(self):
+        return self.datasetName
+
+    def getAttributes(self):
+        return self.attributes
+
+    def getNormalizedAttributes(self):
+        normalizedAttributes = []
+        for attr in self.attributes:
+            normalizedAttributes.append(attr.normalizedName)
+        return normalizedAttributes
+
+    def getNormalizedFDs(self):
+        normalizedFDs = []
+        for fd, label in self.fds:
+            normFD = []
+            for attr in fd:
+                normFD.append(attr.normalizedName)
+            normalizedFDs.append((normFD, label))
+        return normalizedFDs
+
+    def getNormalizedCompositeKeys(self):
+        return self._normalizeList(self.compositeKeys)
+
+    def getFDs(self):
+        return self.fds
+
+    def getDataFrame(self):
+        return self.dataframe
+
+    def getMappingsAttributes(self):
+        return self.mappigsAttributes
+
+    def getAmbiguousAttribute(self):
+        return self.ambiguousAttribute
+
+    def getAmbiguousAttributeNormalized(self):
+        normalizedAttributes = []
+        for attr1, attr2, label in self.ambiguousAttribute:
+            normalizedAttributes.append((attr1.normalizedName, attr2.normalizedName, label))
+        ##return self.ambiguousAttributeNormalized
+        return normalizedAttributes
+
+    def findAmbiguousAttributes(self, strategy, t5Engine):
+        self.ambiguousAttribute = []
+        pairwiseAttributes = []
+        #pairwiseAttributes = list(combinations(self.attributes, 2))
+        pairwiseAttributes = list(permutations(self.attributes, 2))
+        if (strategy == STRATEGY_SCHEMA):
+            linearizedSchema = self._linearizeSchema()
+            for attr1, attr2 in pairwiseAttributes:
+                label = getAmbiguousAttribute(linearizedSchema, attr1, attr2, strategy, t5Engine)
+                if label is not None:
+                    self.ambiguousAttribute.append((attr1, attr2, label))
+        if (strategy == STRATEGY_SCHEMA_WITH_DATA_SAMPLE):
+            schemaWithData = self._schemaWithDataSample() ## TODO code in colab task3
+            for attr1, attr2 in pairwiseAttributes:
+                label = getAmbiguousAttribute(schemaWithData, attr1, attr2, strategy, t5Engine)
+                if label is not None:
+                    self.ambiguousAttribute.append((attr1, attr2, label))
+
+    def storeInDB(self, engine, if_exists='replace'):
+        ## if_exists {'fail', 'replace', 'append'}
+        self.dataframe.to_sql(self.datasetName, engine, if_exists=if_exists)
+        dataframeRows = self.dataframe.shape[0]
+        rowsInDB = engine.execute("SELECT count(*) from "+self.datasetName).fetchall()[0][0]
+        return dataframeRows == rowsInDB
+
+    def _dictNameToAttribute(self):
+        mappings = dict()
+        for attr in self.attributes:
+            mappings[attr.name] = attr
+        return mappings
+
+    def _getAttributesFromDF(self, df):
+        dtypes = df.infer_objects().dtypes
+        attributes = []
+        for attrName, attrType in dtypes.items():
+            type = CATEGORICAL
+            if is_numeric_dtype(attrType):
+                type = NUMERICAL
+            attribute = Attribute(attrName)
+            attribute.setType(type)
+            attributes.append(attribute)
+        return attributes
+
+    def _getAttributes(self, file):
+        df = pd.read_csv(file)
+        dtypes = df.infer_objects().dtypes
+        attributes = []
+        if self.pk == INDEX:
+            attributes.append((INDEX[0], CATEGORICAL))
+        for attrName, attrType in dtypes.items():
+            type = CATEGORICAL
+            if is_numeric_dtype(attrType):
+                type = NUMERICAL
+            attributes.append((attrName, type))
+        return attributes
+
+    def _normalizeList(self, listOfList):
+        normalizedNames = []
+        for list in listOfList:
+            normalized = []
+            for attr in list:
+                normalized.append(attr.normalizedName)
+            normalizedNames.append(normalized)
+        return normalizedNames
+
+    def _loadDataFrame(self, file):
+        reader = pd.read_csv(file, sep=None, iterator=True, engine='python')
+        inferred_sep = reader._engine.data.dialect.delimiter
+        df = pd.read_csv(file, sep=inferred_sep)
+        return df
+
+    def _linearizeSchema(self):
+        attributeNames = [attr.name for attr in self.attributes]
+        return "|".join(attributeNames)
+
+    def _schemaWithDataSample(self):
+        ## TODO: implement from task 3
+        return ""
+
+    def toJSON(self):
+        delattr(self, "dataframe")
+        return json.dumps(self, default=lambda o: o.__dict__,  indent=4)
+
--- src/rest/PythiaRoute.py
+++ src/rest/PythiaRoute.py
@@ -3,9 +3,22 @@ import os
 
 from fastapi import APIRouter, Depends, UploadFile, Form
 
-from src.pythia.Authentication import User, get_current_active_user
-from src.pythia.DBUtils import getScenarioListFromDb
+from src.pythia.Constants import STRATEGY_SCHEMA
+from src.pythia.T5Engine import T5Engine
+from src.rest.Authentication import User, get_current_active_user
+from src.pythia.DBUtils import getScenarioListFromDb, getScenarioFromDb, deleteScenarioFromDb, existsDTModelInDb, \
+    insertScenario, getEngine, updateScenario
 from src.pythia.Dataset import Dataset
+
+t5Engine = T5Engine().getInstance()
+
+## DB PARAMETER
+# TODO: read from config file
+DB_USER = "postgres"
+DB_PASSWORD = "postgres"
+DB_HOST = "localhost"
+DB_PORT = "5432"
+DB_NAME = "pythia"
 
 pythiaroute = APIRouter()
 
@@ -17,18 +30,89 @@ def getScenarios(user: User = Depends(get_current_active_user)):
         result[row[0]] = row[0].replace(user.username + "_", "")
     return result
 
+@pythiaroute.get("/check/{name}")
+def checkScenario(name: str, user: User = Depends(get_current_active_user)):
+    name = name.replace(".csv", "")
+    name = user.username + "_" + name
+    result = existsDTModelInDb(name)
+    return result
 
-@pythiaroute.post("/uploadFile")
+
+@pythiaroute.post("/create")
 def uploadFile(file: UploadFile = Form(...), user: User = Depends(get_current_active_user)):
     file_location = f"tmp/{file.filename}"
     name = file.filename.replace(".csv", "")
     datasetName = user.username + "_" + name
     with open(file_location, "wb+") as file_object:
         file_object.write(file.file.read())
-    with open(file_location, "r") as file_read:
-        dialect = csv.Sniffer().sniff(file_read.readline(), delimiters=";,")
-        separator = dialect.delimiter
-    dataSet = Dataset(datasetName)
-    dataSet.storeInDB(file_location, separator)
+    dataset = Dataset(datasetName)
+    dataset.initDataframe(file_location)
+    attributes = dataset.getAttributes()
+    df = dataset.getDataFrame()
+    dataset.findPK()
+    engine = getEngine(DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME)
+    checkDB = dataset.storeInDB(engine)
+    print("*** Check DB: ", checkDB)
+    insertScenario(datasetName, user.username, dataset)
+    #dataSet.storeInDB(file_location, user.username, separator)
     os.remove(file_location)
-    return dataSet.df
+    return name
+
+
+@pythiaroute.get("/dataframe/{name}")
+def getDataframe(name: str, user: User = Depends(get_current_active_user)):
+    scenario = getScenarioFromDb(name)
+    ## TODO: return scenario.dataframe.head(10).to_html(classes='table table-striped table-bordered')
+    return scenario.dataframe.to_html(classes='table table-striped table-bordered')
+
+
+@pythiaroute.delete("/delete/{name}")
+def deleteScenario(name: str, user: User = Depends(get_current_active_user)):
+    deleteScenarioFromDb(name)
+
+
+@pythiaroute.get("/get/{name}")
+def getScenario(name: str, user: User = Depends(get_current_active_user)):
+    scenario = getScenarioFromDb(name)
+    return scenario.toJSON()
+
+@pythiaroute.get("/find/cks/{name}")
+def findCompositeKeys(name: str, user: User = Depends(get_current_active_user)):
+    scenario = getScenarioFromDb(name)
+    maxSizeKeys = 3  ## TODO: parameters
+    ckMinimal = False  ## TODO: parameters
+    useNumerical = False ## TODO: parameters
+    scenario.findCompositeKeys(scenario.dataframe, maxSizeKeys, ckMinimal)
+    scenario.pruneCKsByType(useNumerical)
+    updateScenario(name, scenario)
+    scenario = getScenarioFromDb(name) ## TODO: remove issue with delattr in toJSON
+    return scenario.toJSON()
+
+@pythiaroute.get("/find/fds/{name}")
+def findFDs(name: str, user: User = Depends(get_current_active_user)):
+    scenario = getScenarioFromDb(name)
+    scenario.findFDs(scenario.dataframe, True)
+    fds = scenario.getFDs()
+    rowMeaning = ["has", "in"] ## TODO: parameter for the next function
+    scenario.extendFDs(rowMeaning)
+    updateScenario(name, scenario)
+    scenario = getScenarioFromDb(name)  ## TODO: remove issue with delattr in toJSON
+    return scenario.toJSON()
+
+@pythiaroute.get("/find/ambiguous/{name}")
+def findAmbiguous(name: str, user: User = Depends(get_current_active_user)):
+    scenario = getScenarioFromDb(name)
+    strategy = STRATEGY_SCHEMA ## TODO: Parameter
+    scenario.findAmbiguousAttributes(strategy, t5Engine)
+    ambiguousAttributes = scenario.getAmbiguousAttribute()
+    print(len(ambiguousAttributes))
+    updateScenario(name, scenario)
+    scenario = getScenarioFromDb(name)  ## TODO: remove issue with delattr in toJSON
+    return scenario.toJSON()
+
+
+
+
+
+
+
